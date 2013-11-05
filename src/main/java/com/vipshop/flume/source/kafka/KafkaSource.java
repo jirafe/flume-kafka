b73fb6f3664f45398ebfbb40c975b4aa523b8552
John Connor <john.theman.connor@gmail.com>
2013-11-05 13:25:12 -0500

    Reverted back to public to preserve interface.

--- src/main/java/com/vipshop/flume/source/kafka/KafkaSource.java
+++ src/main/java/com/vipshop/flume/source/kafka/KafkaSource.java
@@ -26,40 +26,50 @@ import org.slf4j.LoggerFactory;
 import com.vipshop.flume.KafkaUtil;
 
 public class KafkaSource extends AbstractSource implements Configurable, PollableSource {
+
+	/**
+	 * @param args
+	 */
 	private static final Logger log = LoggerFactory.getLogger(KafkaSource.class);
 	ConsumerConnector consumer;
 	ConsumerIterator<Message> it;
 	String topic;
 	Integer batchSize;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 	public Status process() throws EventDeliveryException {
 		try {
-			ArrayList<Event> eventList = new ArrayList<Event>();
-			Message message;
-			Event event;
-			ByteBuffer buffer;
-			Map<String, String> headers;
-			byte [] bytes;
-			for(int i = 0; i < batchSize; i++) {
-				if(it.hasNext()) {
-					log.trace("-----------------has next");
-					message = it.next().message();
-					event = new SimpleEvent();
-					buffer = message.payload();
-					headers = new HashMap<String, String>();
-					headers.put("timestamp", String.valueOf(System.currentTimeMillis()));
-					bytes = new byte[buffer.remaining()];
-					buffer.get(bytes);
-					event.setBody(bytes);
-					event.setHeaders(headers);
-					log.trace(new String(bytes));
-					eventList.add(event);
-				}
-				log.trace("----------------event list add done");
+		// TODO Auto-generated method stub
+		ArrayList<Event> eventList = new ArrayList<Event>();
+		Message message;
+		Event event;
+		ByteBuffer buffer;
+		Map<String, String> headers;
+		byte [] bytes;
+		for(int i = 0; i < batchSize; i++){
+			if(it.hasNext()) {
+				log.trace("-----------------has next");
+				message = it.next().message();
+				event = new SimpleEvent();
+				buffer = message.payload();
+				headers = new HashMap<String, String>();
+				headers.put("timestamp", String.valueOf(System.currentTimeMillis()));
+				bytes = new byte[buffer.remaining()];
+				buffer.get(bytes);
+				event.setBody(bytes);
+				event.setHeaders(headers);
+				log.trace(new String(bytes));
+				eventList.add(event);
 			}
-			getChannelProcessor().processEventBatch(eventList);
-			log.trace("------------------process event batch");
-			return Status.READY;
+		log.trace("----------------event list add done");
+		}
+		getChannelProcessor().processEventBatch(eventList);
+		log.trace("------------------process event batch");
+		return Status.READY;
 		} catch (Exception e) {
 			log.debug("-----process exception: " + e);
 			return Status.BACKOFF;
@@ -67,30 +77,35 @@ public class KafkaSource extends AbstractSource implements Configurable, Pollabl
 	}
 
 	public void configure(Context context) {
+		// TODO Auto-generated method stub
 		this.topic = KafkaUtil.getTopic(context);
 		try {
 			this.consumer = KafkaUtil.getConsumer(context);
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (KeeperException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		this.batchSize = Integer.parseInt(KafkaUtil.getBatchSize(context));
 		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
 		topicCountMap.put(topic, new Integer(1));
 		Map<String, List<KafkaStream<Message>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-		KafkaStream<Message> stream =  consumerMap.get(topic).get(0);
-		it = stream.iterator();
-		log.debug("-------------init it done");
+	    KafkaStream<Message> stream =  consumerMap.get(topic).get(0);
+	    it = stream.iterator();
+	    log.debug("-------------init it done");
 	}
 
 	@Override
 	public synchronized void stop() {
+		// TODO Auto-generated method stub
 		consumer.shutdown();
 		log.debug("-----------------stop");
 		super.stop();
 	}
-}
 
+}
